609dd3109af908394b42a4775ea95a513a1ab90b
arun02akashr <arun_akashr@tamu.edu>
2025-02-10 17:28:25 -0600

    feature-user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new  # Initialize a new User object
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to dashboard_path, notice: "Account created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
