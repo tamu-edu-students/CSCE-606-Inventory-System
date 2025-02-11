70ad78c4f7535a24c6f32a2e26116686c3af6ea4
arun02akashr <arun_akashr@tamu.edu>
2025-02-10 18:12:00 -0600

    Delete app/controllers/dashboard_controller.rb

--- app/controllers/dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-class DashboardController < ApplicationController
-  before_action :require_login
-
-  #simple for now, only show the username and bins counts 
-  def index
-    @user = current_user
-    @bins_counts = @user.bins.count 
-  end
-end
